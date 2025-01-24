65af9a9bd37b7db6463d20892b8fad169276fdb3
abeer025 <abeeriqbalahmed@gmail.com>
2025-01-24 19:35:20 +0500

    create authentication of user

--- server/index.js
+++ server/index.js
@@ -2,14 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import user from "./routers/user.route.js";
-import course from "./routers/course.route.js";
-import batch from "./routers/batch.route.js";
-import applyCourse from "./routers/applyCourse.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import trainer from './routers/trainer.route.js'
-import assignment from "./routers/assignment.route.js"
-import resourceRoutes from './routers/resources.route.js'
 
 // Load environment variables
 dotenv.config({});
@@ -27,12 +21,6 @@ app.use(cors());
 
 // API Routes
 app.use("/api/v1/user", user);
-app.use("/api/v1/course", course);
-app.use("/api/v1/batch", batch);
-app.use("/api/v1/applyCourse", applyCourse);
-app.use('/api/v1/trainer', trainer);
-app.use("/api/v1/assignments", assignment);
-app.use("/api/v1/resources", resourceRoutes);
 
 // Start the server
 app.listen(PORT, () => {
